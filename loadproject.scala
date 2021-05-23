d5bdf9b087af2091a0e6755f27f691ac467a3de2
Deepthi-S <59824729+DeepthiSudharsan@users.noreply.github.com>
2021-05-23 18:29:13 +0530

    Delete loadproject.scala

--- loadproject.scala
+++
@@ -1,7 +0,0 @@
-// the file which has code for reading the data
-:load readdata.scala
-// the file with the code for analysis of customer information
-:load analysiscustomer.scala
-// the file with the code for analysis of company information
-:load analysiscompany.scala
-
