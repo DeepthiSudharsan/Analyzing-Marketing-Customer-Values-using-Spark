fa2e711a41b75a2311f2e335e96ee902225a54cd
Kavya S Kumar <76042437+kavyasureshkumar@users.noreply.github.com>
2021-05-23 18:33:25 +0530

    Add files via upload

---
+++ loadproject.scala
@@ -0,0 +1,7 @@
+// the file which has code for reading the data
+:load readdata.scala
+// the file with the code for analysis of customer information
+:load analysiscustomer.scala
+// the file with the code for analysis of company information
+:load analysiscompany.scala
+
